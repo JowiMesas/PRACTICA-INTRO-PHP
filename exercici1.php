eac7c12e1727dd42a2b426b45bd79a619f4efaf1
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-18 19:52:54 +0200

    Exercici1.php

---
+++ exercici1.php
@@ -0,0 +1,29 @@
+<?php
+$num1 = 5;
+$num2 = 10;
+ //Operaciones
+ $suma = $num1 + $num2;
+ $resta = $num1 - $num2;
+ $division = $num1 / $num2;
+
+ //Respuestas
+ echo "La suma de $num1 i $num2 es $suma";
+ echo "La resta de $num1 i $num2 es $resta";
+ echo "La division de $num1 i $num2 es $division";
+
+ if ($num1 == $num2) {
+    echo"$num1 i $num2 son iguales";
+ } else if( $num1 > $num2) {
+    echo "$num1 es mas grande que $num2";
+ } else if( $num1 < $num2) {
+    echo "$num2 es mas grande que $num1";
+ }
+ /**
+  * 3. Si las dos variables son valores superiores a 1,
+* a. calcula el área del triángulo con base y altura igual a los valores de las
+ * variables.
+*/
+if( $num1 > 1 && $num2 > 1) {
+    $area = $num1 * $num2 / 2;
+    echo "El area del triangle es $area";
+}
