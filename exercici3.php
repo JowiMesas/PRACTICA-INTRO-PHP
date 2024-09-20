4ddcd52a1d86105cae1693fa78403cb392b1bfd8
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-20 18:47:45 +0200

    exercici 3

--- exercici3.php
+++ exercici3.php
@@ -18,4 +18,14 @@ for ($i = 0; $i < $num1; $i++) {
 echo" <br> 3. Muestra la progresión numérica desde la segunda variable hasta 0 con un bucle
 while. <br>";
 
-
+while ($num2 >= 0) {
+   echo $num2. " ";
+   $num2--;
+}
+echo "<br>  Muestra la progresión numérica desde la primera variable a la segunda con un bucle
+do/while.
+a. Si la segunda variable es más pequeña, sólo queremos que imprima una vez el valor de la primera variable. <br>";
+do {
+echo $num1 . " ";
+$num1++;
+} while ($num1 < $num2);
