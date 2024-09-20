41d9f13f64258a8c77853f51d0de1341f668691b
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-20 18:56:42 +0200

    exercici4

---
+++ exercici4.php
@@ -0,0 +1,7 @@
+<?php
+echo"1. Genere un número aleatorio entre el 1 y el 6 para simular la Ɵrada de un dado. <br>";
+$dado = rand(1,6);
+ echo "El valor es: $dado ";
+ 
+ echo"<br> 2. Muestra en números y letras el valor obtenido y valor que tendrá el dado en la cara
+opuesta. (1:5, 2:6, 3:4) <br>";
