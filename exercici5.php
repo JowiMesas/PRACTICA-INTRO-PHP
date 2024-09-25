e9a3fdc857b40677bb3479739274d83b5b020f88
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-25 20:15:55 +0200

    exercici5

--- exercici5.php
+++ exercici5.php
@@ -5,8 +5,23 @@ echo"<br> 1. Se genere un número aleatorio entre 0 y 20 y se muestre por pantal
 echo "<br> 2. Se repita la operación hasta que el total de valores sumados sea superior a 100. 
 <br>";
 $suma = 0;
+$numeros_pares = 0;
+$numeros_impares = 0;
 while ($suma <= 100) {
     $numero = rand(0,20);
     echo $numero . " | ";
     $suma += $numero;
-}
+
+    if  ($numero % 2 == 0) {
+        $numeros_pares++;
+    } else {
+        $numeros_impares++;
+    }
+    }
+echo "<br> <br> ";
+echo " <br> La suma de valores es $suma";
+
+echo"<br> <br> 4. Muestra el total de números pares e impares generados. 
+<br> ";
+echo" Hay $numeros_pares numeros pares <br> ";
+echo" Hay $numeros_impares numeros impares <br>";
