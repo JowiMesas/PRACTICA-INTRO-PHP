03e87a0b3674e52f7088784d005db8bf5aae25db
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-25 20:04:36 +0200

    suma

--- exercici5.php
+++ exercici5.php
@@ -2,6 +2,11 @@
 
 echo"<br> 1. Se genere un número aleatorio entre 0 y 20 y se muestre por pantalla. <br>";
 
-$numero = rand(0,20);
-
-echo "Numero : $numero <br>";
+echo "<br> 2. Se repita la operación hasta que el total de valores sumados sea superior a 100. 
+<br>";
+$suma = 0;
+while ($suma <= 100) {
+    $numero = rand(0,20);
+    echo $numero . " | ";
+    $suma += $numero;
+}
