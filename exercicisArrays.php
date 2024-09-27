b0231cdb0a5cd21d02ef7223828b8c8907dd55d0
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-27 19:13:19 +0200

    arrays

--- exercicisArrays.php
+++ exercicisArrays.php
@@ -1,2 +1,56 @@
 <?php
 
+echo"1) Crea un array asociativo con los siguientes datos y claves.
+nombre: Sara, apellido: Martinez, edad: 23, ciudad: Barcelona.
+Muestra los valores del array anterior utilizando foreach <br> <br> ";
+$persona = array("nombre" => "Sara","apellido" => "Martinez","edad"=> "23","ciudad" => "Barcelona") ;
+
+$contador = 1;
+foreach ($persona as $valor) {
+echo "dato" .$contador++. "º:". $valor ."<br>";
+
+}
+
+echo "<br> 2) Muestra los valores del array del ejercicio 1 mostrando la clave y el valor utilizando
+foreach. <br> <br>";
+foreach ($persona as $key => $valor) {
+
+echo $key.":". $valor ."<br>";
+}
+
+echo" <br> 3) Modifica la edad del primer array a 24. Vuelve a mostrar toda su información <br> <br>";
+$persona ["edad"] = 24;
+$contador = 1;
+foreach ($persona as $valor) {
+    echo "dato" .$contador++. "º:". $valor ."<br>";
+    
+    }
+echo "<br> 4) Borra la ciudad del array y vuelve a mostrar toda su información usando la función
+var_dump. <br> <br>";
+unset(  $persona["ciudad"]);
+var_dump($persona);
+
+echo "<br> <br>  5) Crear un nuevo array con un valor separado por coma a partir de la cadena de texto
+$ letters = “a,b,c,d,e,f”. Usando la función explode. Muestra su información en orden descendente <br> <br>";
+$letters = "a,b,c,d,e,f";
+$array = explode(",", $letters);
+rsort($array);
+$contadorDesc = 6;
+
+foreach ($array as $letra) {
+echo "letter ".$contadorDesc--."º:" .$letra. "<br>";
+}
+echo "<br> <br> 6) Un profesor quiere registrar las notas de su clase en un array asociativo. Las notas son
+las siguientes: <br>
+Miguel: 5, Luís: 7, Marta: 10, Isabel: 8, Aitor: 4, Pepe: 1 <br>
+Guarda los datos en un array asociativo en el orden previo y muéstralos ordenados de
+mayor a menor. <br> <br>";
+
+$array_notas = array("Miguel" => "5","Luis" => "7","Marta" => "10","Isabel" => "8","Aitor" => "4","Pepe" => "1");
+arsort( $array_notas );
+echo "Notas de los estudiantes:";
+foreach ($array_notas as $nombreAlumno => $notaAlumno) {
+echo "" .$nombreAlumno . ":" .$notaAlumno. " ";
+}
+echo "<br> <br> 7) Calcula la media de las notas y muéstrala con solo 2 decimales. Además, muestra los
+nombres de los alumnos cuya nota esté por encima de la media. <br> <br>";
