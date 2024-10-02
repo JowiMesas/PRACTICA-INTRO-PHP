55eb082ebd3e353c07a0836a81d6cb9b97c2e96b
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-10-02 20:22:26 +0200

    sessions

--- ExerciseSession01.php
+++ ExerciseSession01.php
@@ -4,6 +4,44 @@ b) Se debe poder añadir y quitar leche o refresco seleccionando de una lista
 c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese
 caso mostrar error. */
 session_start();
+//Inicializamos las variables por si aun  no existe aun el trabajador
+if (!isset($_SESSION['worker'])) {
+    $_SESSION['worker'] = '';
+}
+if (!isset($_SESSION['milk'])) {
+    $_SESSION['milk'] = 0;
+}
+if (!isset($_SESSION['softDrink'])) {
+    $_SESSION['softDrink'] = 0;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $_SESSION['worker'] = $_POST['worker'];
+    $tipoProducto = $_POST['lista'];
+    $quantity = intval($_POST['quantity']);
+
+    //Añadimos la cantidad puesta en el tipo de Producto que se elija
+if (isset($_POST['add'])) {
+    if ($tipoProducto == 'milk') {
+        $_SESSION['milk'] += $quantity;
+    } else if ($tipoProducto == 'softDrink') {
+        $_SESSION['softDrink'] += $quantity;
+    }
+ } else if(isset($_POST['remove'])) {
+    if ($tipoProducto == 'milk') {
+        if($_SESSION['milk'] >= $quantity) {
+            $_SESSION['milk'] -= $quantity;
+        } else {
+            echo "<p> ERROR!! No se puede quitar mas unidades de las que  hay!! </p>";
+        }
+    } else if($tipoProducto == 'softDrink') {
+        if($_SESSION['softDrink'] >= $quantity) {
+            $_SESSION['softDrink'] -= $quantity;
+        } else {
+            echo '<p> ERROR!! No se puede quitar mas unidades de las que  hay!! </p> ';
+        }
+    }
+ }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +53,7 @@ session_start();
 <body>
  <form action="" method="post">
  <h2>Supermarket management</h2>
-    Worker name: <input type="text" name="quantity" required>
+    Worker name: <input type="text" name="worker" required>
     <br>
     <h3>Choose product: </h3>
     <select name="lista" id="lista">
@@ -28,7 +66,11 @@ session_start();
     <br> <br>
     <input type="submit" name="add" value="add" >
     <input type="submit" name="remove" value="remove" >
-    <input type="submit"  name="reset" value="reset" >
+    <input type="reset"  name="reset" value="reset" >
  </form>
+ <h3>Inventary:</h3>
+ <p>Worker: <?php echo $_SESSION['worker']; ?> </p>
+ <p>Units milk: <?php echo $_SESSION['milk']; ?> </p>
+ <p>Units soft drink: <?php echo $_SESSION['softDrink']; ?> </p>
 </body>
 </html>
